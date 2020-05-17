8cbab982b1b8fa40cd6e4c718ebc6e40d493b92a
Kowndinya2000 <cs17b032@iittp.ac.in>
2020-05-17 19:10:09 +0530

    java compiler fix

---
+++ java_program.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+    var code = 'class Simple{public static void main(String args[]){System.out.println("Hello Java");}}'
+document.querySelectorAll(".ace_content")[0].innerHTML = code
+}, 2000);
